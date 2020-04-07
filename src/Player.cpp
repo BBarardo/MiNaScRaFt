b0f133967edc8a1f9a021373a826d1856bade600
Bernardo Barardo <Bernardo.s.barardo@hotmail.com>
2020-04-07 18:22:14 +0100

    Add project files.

---
+++ src/Player.cpp
@@ -0,0 +1,19 @@
+﻿#include "Player.h"
+
+
+Player::Player(float _posX, float _posY, float _posZ):camera(Camera(glm::vec3(0.0f, 2.0f, 0.0f)))
+{
+	posX = _posX;
+	posY = _posY;
+	posZ = _posZ;
+}
+
+Player::~Player()
+{
+	
+}
+
+void Player::move()
+{
+	
+}
---
+++ src/Player.h
@@ -0,0 +1,21 @@
+﻿#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <camera.h>
+
+class Player
+{
+	float posX;
+	float posY;
+	float posZ;
+	Camera camera;
+	
+public:
+	Player();
+	Player(float _posX = 0, float _posY = 0, float _posZ = 0);
+	~Player();
+	void move();
+};
+
+#endif
+
